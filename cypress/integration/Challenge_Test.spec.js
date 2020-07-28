3f01582fc9d2a1305507e51fc5dd9a0cbcdf341d
hedmon84 <hedmonlopes@gmail.com>
2020-07-28 16:08:41 -0600

    choose a parking

--- cypress/integration/Challenge_Test.spec.js
+++ cypress/integration/Challenge_Test.spec.js
@@ -3,5 +3,15 @@
 describe("First state test", () => {
   it("Firts Test", () => {
     cy.openMainPage();
+
+    cy.get("form")
+      .find("tbody")
+      .then(tablerow => {
+        cy.wrap(tablerow)
+          .find("tr")
+          .first()
+          .get("select")
+          .select("Short-Term Parking");
+      });
   });
 });
