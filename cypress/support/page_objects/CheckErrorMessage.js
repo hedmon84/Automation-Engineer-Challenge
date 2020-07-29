918171f8e7a1c0984cb735e844d1c7f266f43c02
hedmon84 <hedmonlopes@gmail.com>
2020-07-29 10:08:33 -0600

    las commit

--- cypress/support/page_objects/CheckErrorMessage.js
+++ cypress/support/page_objects/CheckErrorMessage.js
@@ -1,12 +1,11 @@
 export class CheckErrorMessage {
   CheckErrMessage(shouldbe) {
-    cy.get("body > form > table > tbody > tr:nth-child(4) > td.SubHead > b")
-    .then(
-      erorr => {
-        const erromessage = eror.text().trim();
-        expect(erromessage).to.equal(shouldbe);
-      }
-    );
+    cy.get(
+      "body > form > table > tbody > tr:nth-child(4) > td.SubHead > b"
+    ).then(erorr => {
+      const erromessage = erorr.text().trim();
+      expect(erromessage).to.equal(shouldbe);
+    });
   }
 }
 export const OnerrMessage = new CheckErrorMessage();
